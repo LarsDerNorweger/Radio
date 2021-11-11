001540e8a6f763cb26f8944f9e2b307d6637a004
LarsDerNorweger <boettger.colin@gmail.com>
2021-11-11 14:56:23 +0100

    Correct and Optimize

--- Internal/restart.sh
+++ Internal/restart.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+cd ~/Radio/Internal
 read P < ./PID.txt
 kill $P
 echo "[$(date)] Process $P killed and restarted" >> ../Restart.log
-python3 ./InternalOperation.py
+python3 ../main.py
 
--- Internal/start.sh
+++ Internal/start.sh
@@ -2,9 +2,11 @@
 
 logFile=../Restart.log
 
+cd ~/Radio/Internal
+
 echo >> $logFile
 echo "System Restarted" >> $logFile
 echo >> $logFile
 echo "[$(date)] inital Process Started" >> $logFile
 
-python3 ./InternalOperation.py
+python3 ../main.py
