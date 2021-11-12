464a54b47ab95dc18f92786432a29f0d6bd46035
LarsDerNorweger <boettger.colin@gmail.com>
2021-11-12 09:48:20 +0100

    Add Shedule for Content

--- LCD_Display/LCDDisplayManager.py
+++ LCD_Display/LCDDisplayManager.py
@@ -7,7 +7,9 @@
 #-------------------------------------------------------------------------------------------------#
 
 import json
+from os import SEEK_CUR
 from time import time
+import RPi.GPIO as GPIO
 from LCD_Display.LCDSpaceArranger import SpaceArranger
 from LCD_Display.LCDDisplayDriver import DisplayDriver
 
@@ -24,14 +26,28 @@ class DisplayManager:
         self.lineContent = []
         self.shownContent = []
         self.Updated = []
+        self.Shedule = {}
 
         for i in range(0, self.Settings["LCD_LINES"]):
             self.lineContent.append('')
             self.shownContent.append('')
             self.Updated.append(True)
+    
+    async def toggleDisplay(self):
+        self.Displaydriver.ToggleDisplay()
+
+    async def addShedule(self,textfunktion,lineNumber, refreshtime):
+        self.Shedule.update({lineNumber:[textfunktion,refreshtime,0]})
+
+    async def LookForShedule(self):
+        if len(self.Shedule.keys()) != 0:
+            for line in self.Shedule.keys():
+                cache = self.Shedule[line]
+                if float(time())-cache[2] >= cache[1]:
+                    await self.addContentToDisplay(await cache[0](),line)
+                    self.Shedule[line][2] = float(time())
 
     async def addContentToDisplay(self,text,lineNumber):
-
         if lineNumber > self.Settings["LCD_LINES"] or lineNumber == None:
             raise Exception("No defiend Displayline")
 
@@ -42,7 +58,8 @@ class DisplayManager:
             self.Updated[lineNumber-1]=True
 
     async def UpdateDisplay(self):
-        
+        await self.LookForShedule()
+
         if int(time())-self.lastUpdateTime >= self.Settings["LCD_UPDATE_INTERVALL"]:
             for i in range(0,len(self.lineContent)):
                 if self.shownContent[i] != self.lineContent[i] or self.Updated[i]:
@@ -58,3 +75,6 @@ class DisplayManager:
         if len(text)>self.Settings["LCD_WIDTH"]:
             text = text[len(text)-1] + text[0:len(text)-1]
         return text
+
+
+    
